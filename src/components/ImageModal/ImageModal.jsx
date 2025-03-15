c4366b46ccdbbfd73b7d96e66162133ff5aa7bf1
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-03-15 23:34:32 +0200

    r4

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -51,34 +51,3 @@ function ImageModal({ isOpen, onClose, image }) {
 }
 
 export default ImageModal;
-
-// import Modal from 'react-modal';
-
-// Modal.setAppElement('#root');
-
-// function ImageModal({ isOpen, onClose, image }) {
-//   return (
-//     <Modal
-//       isOpen={isOpen}
-//       onRequestClose={onClose}
-//       contentLabel="Image Modal"
-//       style={{
-//         overlay: { backgroundColor: 'rgba(0, 0, 0, 0.75)' },
-//         content: {
-//           maxWidth: '80vw',
-//           maxHeight: '80vh',
-//           margin: 'auto',
-//           padding: 0,
-//         },
-//       }}
-//     >
-//       <img
-//         src={image.urls.regular}
-//         alt={image.alt_description}
-//         style={{ width: '100%' }}
-//       />
-//     </Modal>
-//   );
-// }
-
-// export default ImageModal;
